89a94b0623f62b596c75552f8151a57dfa74e49d
frabot <rabot.fanny@gmail.com>
2018-04-24 15:56:26 +0200

    Added repository temoignage

---
+++ src/AppBundle/Repository/TemoignageRepository.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Fanny
+ * Date: 24/04/2018
+ * Time: 15:28
+ */
+
+namespace AppBundle\Repository;
+
+
+class TemoignageRepository extends \Doctrine\ORM\EntityRepository
+{
+
+}
