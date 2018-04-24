bbbaedefd4ca5cf1413458a25c8304b6e6808ce7
frabot <rabot.fanny@gmail.com>
2018-04-24 15:27:59 +0200

    Added new repositories

---
+++ src/AppBundle/Repository/BateauRepository.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Fanny
+ * Date: 24/04/2018
+ * Time: 15:08
+ */
+
+namespace AppBundle\Repository;
+
+
+class BateauRepository extends \Doctrine\ORM\EntityRepository
+{
+
+}
